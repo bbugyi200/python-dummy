2265cda6acd9f729786ac1e230fc22d3c21816ce
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-09-20 11:57:04 -0400

    Update cc via 'cruft update'

--- src/bugyi/dummy/__init__.py
+++ src/bugyi/dummy/__init__.py
@@ -1,5 +1,5 @@
 """Dummy python project used to test the cc-python cookiecutter."""
 
-__author__ = """Bryan M Bugyi"""
+__author__ = "Bryan M Bugyi"
 __email__ = "bryanbugyi34@gmail.com"
 __version__ = "0.3.2"
